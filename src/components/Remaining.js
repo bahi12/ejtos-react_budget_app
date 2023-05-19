daf62595fe0a990b3cc8d99ffe89bde3d5384d27
Hassan EL BAHI <me_80_me@hotmail.com>
2023-05-19 22:56:19 +0100

    Writing Reamining.js component

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,9 +1,14 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Remaining = () => {
+    const {budget, expenses} = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return(total += item.cost);
+    }, 0);
+    const remaining = budget - totalExpenses;
     return (
         <div className='alert alert-primary'>
-            <span>Remaining</span>
+            <span>Remaining: ${remaining}</span>
         </div>
     );
 };
