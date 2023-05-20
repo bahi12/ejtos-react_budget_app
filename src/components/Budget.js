1c5e7dfb66a4661c95e143c5afc901ae744fe86a
Hassan EL BAHI <me_80_me@hotmail.com>
2023-05-20 14:39:59 +0100

    Attempt to fix the NaN bug

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,14 +1,46 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useState, useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
 const Budget = () => {
-    const { expenses, Location } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total += (item.unitprice * item.quantity));
-    }, 0);
-    return (
-        <div className='alert alert-primary'>
-            <span>Budget: {Location}{totalExpenses}</span>
-        </div>
-    );
+  const { expenses, Location, dispatch } = useContext(AppContext);
+  const [budget, setBudget] = useState("");
+
+  const handleBudgetChange = (event) => {
+    const value = event.target.value;
+    if (!isNaN(value)) {
+      setBudget(value);
+    }
+  };
+
+  const handleSaveBudget = () => {
+    dispatch({
+      type: "SET_BUDGET",
+      payload: parseInt(budget),
+    });
+  };
+
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.unitprice * item.quantity);
+  }, 0);
+
+  const budgetValue = isNaN(budget)
+    ? "No budget set yet"
+    : Location + parseInt(budget);
+
+  return (
+    <div className="alert alert-primary">
+      <span className="budget-label">Budget: </span>
+      <input
+        type="text"
+        className="budget-value"
+        value={budget}
+        onChange={handleBudgetChange}
+        placeholder="Enter budget"
+      />
+      <span>{isNaN(budgetValue) ? "" : budgetValue}</span>
+      <button onClick={handleSaveBudget}>Save</button>
+    </div>
+  );
 };
+
 export default Budget;
--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,10 +1,26 @@
-import React, { useContext } from 'react';
-//import { AppContext } from '../context/AppContext';
-const ExpenseTotal = () => {
-    return (
-        <div className='alert alert-primary'>
-            <span>Totl expense</span>
-        </div>
-    );
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
+const TotalExpense = () => {
+  const { expenses, Location } = useContext(AppContext);
+
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.unitprice * item.quantity);
+  }, 0);
+
+  const formattedTotalExpenses = isNaN(totalExpenses)
+    ? "0"
+    : totalExpenses.toFixed(2);
+
+  return (
+    <div className="alert alert-secondary">
+      <span className="total-expense-label">Total Expenses: </span>
+      <span className="total-expense-value">
+        {Location}
+        {formattedTotalExpenses}
+      </span>
+    </div>
+  );
 };
-export default ExpenseTotal;
+
+export default TotalExpense;
